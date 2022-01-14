afa39225fa49ed70ff549cf748ddf360b56e4811
wtaisner <taisner.witold@gmail.com>
2022-01-14 12:35:38 +0100

    added dataset implementation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import numpy as np
 
 # see if venv is correctly installed
 import torch
 x = torch.rand(5, 3)
 print(x)
 print(torch.cuda.is_available())
+
+file = np.load('data/example_test.npy')
+print(file.shape, type(file))
